5610c9d2d60e9f4f9e9bdcdb4dbdf4a62c8b0618
Andreil <guff-0000@mail.ru>
2024-03-23 13:34:57 +0300

    abb flail

--- mail/stail.js
+++ mail/stail.js
@@ -5,5 +5,5 @@ function openModal() {
     document.getElementById("modal").style.top = "0px";
 }
 function closeModal() {
-    document.getElementById("modal").style.top = "-500px";
+    document.getElementById("modal").style.top = "-300px";
 }
